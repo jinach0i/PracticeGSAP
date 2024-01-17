344f37f6dbca9cfd26a907182bd27df405006883
jinach0i <ginach0irk@gmail.com>
2024-01-17 22:52:13 +0900

    first commit

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import 그림1 from '../public/images/img (1).jpg';
+import 그림2 from '../public/images/img (2).jpg';
+import 그림3 from '../public/images/img (3).jpg';
+import 그림4 from '../public/images/img (4).jpg';
+import 그림5 from '../public/images/img (5).jpg';
+import 그림6 from '../public/images/img (6).jpg';
+import 그림7 from '../public/images/img (7).jpg';
+import 그림8 from '../public/images/img (8).jpg';
+export {
+    그림1,
+    그림2,
+    그림3,
+    그림4,
+    그림5,
+    그림6,
+    그림7,
+    그림8
+}
